1beef95e47cb119739292196c975e66871fb5121
CyberCookie <olek.wiesiolek@gmail.com>
2025-01-18 21:36:17 +0100

    Update main_code.py
    Many changes

--- handytools/internal_code/main_code.py
+++ handytools/internal_code/main_code.py
@@ -3,16 +3,22 @@ from script_handling import run_outside_script
 import os
 import platform
 import json
-from pathlib import Path
 import shutil
+import internal_vital_scripts
+import datetime
 
 file_list = []
 downloaded_scripts = ""
 path = "scripts"
+license = f"""
+Copyright {str(datetime.date.year)} <COPYRIGHT HOLDER>
 
-import os
-import shutil
-import json
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 
 def add_script(add_file_path):
@@ -22,7 +28,9 @@ def add_script(add_file_path):
         target_dir = os.path.join(path, file_name)
         os.makedirs(target_dir, exist_ok=True)
 
-        shutil.move(add_file_path, os.path.join(target_dir, os.path.basename(add_file_path)))
+        shutil.move(
+            add_file_path, os.path.join(target_dir, os.path.basename(add_file_path))
+        )
 
         json_file_path = os.path.join(target_dir, f"{file_name}.json")
         data = {
@@ -32,6 +40,37 @@ def add_script(add_file_path):
         }
         with open(json_file_path, "w") as json_file:
             json.dump(data, json_file, indent=4)
+        internal_vital_scripts.generate_requirements(add_file_path)
+        with open(os.path.join(target_dir, f"LICENSE.txt")) as f:
+            f.write(license)
+        print("Added the default MIT License.")
+        internal_vital_scripts.format_file(f"scripts/{file_name}/{file_name}.py")
+
+        print("Remember to change the main command and data in the JSON file!")
+    else:
+        print("Something went wrong. Please provide a valid Python script path.")
+def add_dir(add_file_path,file_name):
+    if os.path.isfile(add_file_path):
+        target_dir = os.path.join(path, file_name)
+        os.makedirs(target_dir, exist_ok=True)
+        for file in os.listdir(add_file_path):
+            shutil.move(
+                f"{add_file_path}/{file}", f"{target_dir}"
+            )
+
+        json_file_path = os.path.join(target_dir, f"{file_name}.json")
+        data = {
+            "desc": "Temporary description, replace with actual description.",
+            "title": f"Temporary name, {file_name}",
+            "main_command": f"main_loop_{file_name}",
+        }
+        with open(json_file_path, "w") as json_file:
+            json.dump(data, json_file, indent=4)
+        internal_vital_scripts.generate_requirements(add_file_path)
+        with open(os.path.join(target_dir, f"LICENSE.txt")) as f:
+            f.write(license)
+        print("Added the default MIT License.")
+        internal_vital_scripts.format_file(f"scripts/{file_name}/{file_name}.py")
 
         print("Remember to change the main command and data in the JSON file!")
     else:
@@ -43,10 +82,13 @@ def run_settings():
     while u_input not in ["y", "n"]:
         u_input = input("Enable developer mode? [y/n]: ")
     if u_input == "y":
+        print("Please restart the script again to apply changes.")
         return "true"
     if u_input == "n":
+        print("Please restart the script again to apply changes.")
         return "false"
-    print("Please restart the script again to apply changes.")
+
+
 def clear_screen():
     # Check the operating system and clear accordingly
     if platform.system() == "Windows":
@@ -54,16 +96,15 @@ def clear_screen():
     else:
         os.system("clear")  # For MacOS and Linux
 
+
 with open("settings.json", "r") as jsonFile:
     data = json.load(jsonFile)
     if data["dev_mode"] == "true":
-            dev_commands = (
-        """
+        dev_commands = """
         Dev Commands:
                     
         - Enter "add" to add a python script.
         """
-                    )
     else:
         dev_commands = ""
 
@@ -74,9 +115,8 @@ for file in os.listdir(path):
 
 for file in file_list:
     if file.endswith(".txt") == False:
-        downloaded_scripts += (
-            f"- {file}" + "\n" + "        "
-        )
+        downloaded_scripts += f"- {file}" + "\n" + "        "
+
 
 def main_loop():
     dev_mode = False
@@ -85,36 +125,41 @@ def main_loop():
         clear_screen()
 
         menu_text = (
-                frg.LIGHTGREEN_EX
-                + "HandyTools\n"
-                + frg.WHITE
-                + """Welcome to HandyTools, your script toolbox with many useful utility programs!
+            frg.LIGHTGREEN_EX
+            + "HandyTools\n"
+            + frg.WHITE
+            + """Welcome to HandyTools, your script toolbox with many useful utility programs!
 
         Please enter the program's name to go to its dedicated page, enter 'about' to read about
         HandyTools software, enter 'settings' to go to settings, or enter 'exit' to exit.
 
         Currently installed scripts:
         """
-                + downloaded_scripts
-                + """
+            + downloaded_scripts
+            + """
         Commands:
         
         - Enter "exit" to exit.
         - Enter "about" to read about the project
         - Enter "settings" to open settings.
         """
-                + dev_commands
-                + frg.CYAN
-                + "--------------------\n"
-                + frg.MAGENTA
-                + "Author: CyberCookieDev\n"
-                + frg.WHITE
+            + dev_commands
+            + frg.CYAN
+            + "--------------------\n"
+            + frg.MAGENTA
+            + "Author: CyberCookieDev\n"
+            + frg.WHITE
         )
 
         # Print the menu
         print(menu_text)
         answer = input("Enter one of the commands listed above: ")
-        while answer not in file_list and answer not in ["about", "exit", "settings", "add"]:
+        while answer not in file_list and answer not in [
+            "about",
+            "exit",
+            "settings",
+            "add",
+        ]:
             answer = input("Enter one of the commands listed above: ")
         if answer == "about":
             clear_screen()
@@ -160,7 +205,15 @@ def main_loop():
             with open("settings.json", "w") as jsonFile:
                 json.dump(data, jsonFile, indent=4)
         elif answer == "add":
-            u_input = input("Paste filepath to the python file you want to add: ")
-            add_script(u_input)
+            u_input = input("Add directory or file? [dir/file]: ")
+            while u_input not in ["dir", "file"]:
+                u_input = input("Add directory or file? [dir/file]: ")
+            if u_input == "dir":
+                u_input = input("Paste filepath to the directory you want to add: ")
+                x = input("What is the name of the main file?: ")
+                add_dir(u_input, x)
+            else:
+                u_input = input("Paste filepath to the python file you want to add: ")
+                add_script(u_input)
 
 main_loop()
