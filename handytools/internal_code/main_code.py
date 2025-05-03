98ea1c2b99129c7f01f6bc60794e265ee070a63a
CyberCookie <olek.wiesiolek@gmail.com>
2025-05-03 14:20:11 +0200

    Small imporvements
    Now, the main command is main() + introduced small changes like comments or rephrasing.

--- handytools/internal_code/main_code.py
+++ handytools/internal_code/main_code.py
@@ -7,6 +7,7 @@ import shutil
 import internal_vital_scripts
 import datetime
 
+
 file_list = []
 downloaded_scripts = ""
 path = "../../scripts"
@@ -22,6 +23,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 
 def add_script(add_file_path):
+
+    # Adds temporary json file with description, temporary name, and the default license (MIT License).
+
     if os.path.isfile(add_file_path) and add_file_path.endswith(".py"):
         file_name = os.path.splitext(os.path.basename(add_file_path))[0]
 
@@ -36,57 +40,77 @@ def add_script(add_file_path):
         data = {
             "desc": "Temporary description, replace with actual description.",
             "title": f"Temporary name, {file_name}",
-            "main_command": f"main_loop_{file_name}",
         }
+
         with open(json_file_path, "w") as json_file:
             json.dump(data, json_file, indent=4)
         internal_vital_scripts.generate_requirements(add_file_path)
+
         with open(os.path.join(target_dir, f"LICENSE.txt")) as f:
             f.write(license)
         print("Added the default MIT License.")
         internal_vital_scripts.format_file(f"../../scripts/{file_name}/{file_name}.py")
 
         print("Remember to change the main command and data in the JSON file!")
+
     else:
         print("Something went wrong. Please provide a valid Python script path.")
 
-def add_dir(add_file_path,file_name):
-    if os.path.isfile(add_file_path):
+
+def add_dir(add_file_path, file_name):
+
+    # Repeatedly transfers files.
+    if os.path.isdir(add_file_path):
+
         target_dir = os.path.join(path, file_name)
         os.makedirs(target_dir, exist_ok=True)
+
         for file in os.listdir(add_file_path):
-            shutil.move(
-                f"{add_file_path}/{file}", f"{target_dir}"
-            )
+            shutil.move(f"{add_file_path}/{file}", f"{target_dir}")
 
         json_file_path = os.path.join(target_dir, f"{file_name}.json")
         data = {
             "desc": "Temporary description, replace with actual description.",
             "title": f"Temporary name, {file_name}",
-            "main_command": f"main_loop_{file_name}",
         }
         with open(json_file_path, "w") as json_file:
             json.dump(data, json_file, indent=4)
+
         internal_vital_scripts.generate_requirements(add_file_path)
-        with open(os.path.join(target_dir, f"LICENSE.txt")) as f:
+
+        with open(os.path.join(target_dir, f"LICENSE.txt"), "w+") as f:
             f.write(license)
         print("Added the default MIT License.")
         internal_vital_scripts.format_file(f"../../scripts/{file_name}/{file_name}.py")
 
         print("Remember to change the main command and data in the JSON file!")
+
     else:
         print("Something went wrong. Please provide a valid Python script path.")
 
 
+# Temporarily not working function to import things from GitHub
+def add_from_github(git_url):
+    print(
+        "Please, ensure that the project you import is compatible with our standards."
+    )
+    u_input = input("Enter the github repositories link: ")
+    x = input("Please enter the repositories name: ")
+    print(
+        "Note that if something isn't working, it probably might be the structure, please read the instructions.txt!"
+    )
+
+
+# Really basic function, kind of self explanatory.
 def run_settings():
     u_input = input("Enable developer mode? [y/n]: ")
     while u_input not in ["y", "n"]:
         u_input = input("Enable developer mode? [y/n]: ")
     if u_input == "y":
-        print("Please restart the script again to apply changes.")
+        print("Please start the script again to apply changes.")
         return "true"
     if u_input == "n":
-        print("Please restart the script again to apply changes.")
+        print("Please start the script again to apply changes.")
         return "false"
 
 
@@ -98,6 +122,7 @@ def clear_screen():
         os.system("clear")  # For MacOS and Linux
 
 
+# Enables the visibility of dev commands based on the JSON file, that is changed by the run_settings()
 with open("settings.json", "r") as jsonFile:
     data = json.load(jsonFile)
     if data["dev_mode"] == "true":
@@ -108,17 +133,15 @@ with open("settings.json", "r") as jsonFile:
         """
     else:
         dev_commands = ""
-
+# Adds files to file list
 for file in os.listdir(path):
     validity = True
     if file.endswith(".txt") == False:
         file_list.append(file)
-
-for file in file_list:
-    if file.endswith(".txt") == False:
         downloaded_scripts += f"- {file}" + "\n" + "        "
 
 
+# Main loop of the whole program
 def main_loop():
     dev_mode = False
     to_continue = True
@@ -128,14 +151,12 @@ def main_loop():
             frg.LIGHTGREEN_EX
             + "HandyTools\n"
             + frg.WHITE
-            + """Welcome to HandyTools, your script toolbox with many useful utility programs!
+            + """
+            Welcome to HandyTools, your script toolbox with many useful utility programs!
 
         Please enter the program's name to go to its dedicated page, enter 'about' to read about
         HandyTools software, enter 'settings' to go to settings, or enter 'exit' to exit.
-
-        Currently installed scripts:
         """
-            + downloaded_scripts
             + """
         Commands:
         
@@ -144,6 +165,10 @@ def main_loop():
         - Enter "settings" to open settings.
         """
             + dev_commands
+            + """
+        Currently installed scripts:
+        """
+            + downloaded_scripts
             + frg.CYAN
             + "--------------------\n"
             + frg.MAGENTA
@@ -153,7 +178,9 @@ def main_loop():
 
         # Print the menu
         print(menu_text)
+        # Answer validity check, if valid, looks at the input and runs corresponding code.
         answer = input("Enter one of the commands listed above: ")
+
         while answer not in file_list and answer not in [
             "about",
             "exit",
@@ -161,12 +188,13 @@ def main_loop():
             "add",
         ]:
             answer = input("Enter one of the commands listed above: ")
+
         if answer == "about":
             clear_screen()
             print(
                 frg.LIGHTCYAN_EX
                 + """
-                                        ==================== About ====================
+                                        ==================== About =====================
                 """
                 + frg.WHITE
                 + """
@@ -187,15 +215,18 @@ def main_loop():
                 """
                 + frg.LIGHTCYAN_EX
                 + """
-                                        --------------------------------------------
+                                        ==================================================
                 """
             )
+        # Breaks the while loop terminating the script.
         elif answer == "exit":
             clear_screen()
             to_continue = False
+
         elif answer in file_list:
             clear_screen()
             run_outside_script(answer)
+
         elif answer == "settings":
             dev_mode = run_settings()
             # Read the JSON file
@@ -209,14 +240,18 @@ def main_loop():
         elif answer == "add":
             clear_screen()
             u_input = input("Add directory or file? [dir/file]: ")
+
             while u_input not in ["dir", "file"]:
                 u_input = input("Add directory or file? [dir/file]: ")
+
             if u_input == "dir":
                 u_input = input("Paste filepath to the directory you want to add: ")
                 x = input("What is the name of the main file?: ")
                 add_dir(u_input, x)
+
             else:
                 u_input = input("Paste filepath to the python file you want to add: ")
                 add_script(u_input)
+
 
 main_loop()
